fb87e4c639b260c4bc4640751b75bdffb1d77a74
Leyren <leyren.dev@gmail.com>
2020-02-04 11:13:47 +0100

    Fix gitignore

---
+++ commands/config.js
@@ -0,0 +1,35 @@
+const format = require("../modules/format.js");
+const Command = require("../cmd.js");
+
+const config = new Command();
+
+config.setName("config")
+  .setEnabled(true)
+  .setGuildOnly(true)
+  .setAliases([])
+  .setPermLevel("Administrator")
+  .setCategory("System")
+  .setDescription("View or restore settings for your server.");
+
+  config.default = function(ctx){
+    let embed = format.embed().setTitle("Server Settings");
+
+    Object.entries(ctx.settings).forEach(([key, value]) => {
+      embed.addField(key, format.prettyPrint(value), true);
+    });
+    ctx.message.channel.send(embed);
+  }
+
+config.on("restore", "Reset all settings back to default", async function(ctx){
+  const response = await ctx.self.awaitConfirmation(ctx.message, `Are you sure you want to restore the default configuration?`);
+  if (response) {
+    ctx.self.settings.delete(ctx.guild.id);
+    ctx.message.reply(`Successfully restored default configuration.`);
+  } else {
+    ctx.message.reply(`Action cancelled.`);
+  }
+});
+
+module.exports = config;
+
+
