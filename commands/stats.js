9e5a718889075c7dc8c03092f2be0d9e200baa4e
Wyn <841141+TheWyn@users.noreply.github.com>
2020-11-14 01:41:03 -0500

    Update stats.js
    v12

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
-const { version } = require("discord.js");
+const {version} = require("discord.js");
 const format = require("../modules/format.js");
 const moment = require("moment");
 require("moment-duration-format");
@@ -7,28 +7,28 @@ const Command = require("../modules/Command.js");
 const stats = new Command();
 
 stats.setName("stats")
-  .setEnabled(true)
-  .setGuildOnly(false)
-  .setAliases([])
-  .setPermLevel("User")
-  .setCategory("Misc")
-  .setDescription("Gives some useful bot statistics.");
+    .setEnabled(true)
+    .setGuildOnly(false)
+    .setAliases([])
+    .setPermLevel("User")
+    .setCategory("Misc")
+    .setDescription("Gives some useful bot statistics.");
 
 stats.default = async (ctx) => {
-  const embed = format.embed();
-  
-  const duration = moment.duration(ctx.self.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  embed.setTitle("Statistics")
-    .addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
-    .addField("Uptime", duration, true)
-    .addBlankField(true)
-    .addField("Users", ctx.self.users.size.toLocaleString(), true)
-    .addField("Servers", ctx.self.guilds.size.toLocaleString(), true)
-    .addField("Channels", ctx.self.channels.size.toLocaleString(), true)
-    .addField("Running on", `Discord.js v${version}\nNode ${process.version}`)
-    .addField("Source Code", `[GitHub Link](https://github.com/Leyren/bdo-guildbot)`)
-    .setFooter("Developed by: Leyren#3099");
-  ctx.message.channel.send(embed);
+    const embed = format.embed();
+
+    const duration = moment.duration(ctx.self.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+    embed.setTitle("Statistics")
+        .addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
+        .addField("Uptime", duration, true)
+        .addField('\u200b', '\u200b')
+        .addField("Users", ctx.self.users.cache.size.toLocaleString(), true)
+        .addField("Servers", ctx.self.guilds.cache.size.toLocaleString(), true)
+        .addField("Channels", ctx.self.channels.cache.size.toLocaleString(), true)
+        .addField("Running on", `Discord.js v${version}\nNode ${process.version}`)
+        .addField("Source Code", `[GitHub Link](https://github.com/TheWyn/bdo-questbot)`)
+        .setFooter("Developed by: Leyren#3099 / Wyn#0004");
+    await ctx.message.channel.send(embed);
 };
 
 
