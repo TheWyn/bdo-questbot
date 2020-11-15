4baf171c827605dda89f1a924d8dd77cc001d849
Wyn <841141+TheWyn@users.noreply.github.com>
2020-11-14 23:46:41 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
+const pre = require("../default.js");
+
 module.exports = async client => {
     // Log that the bot is online.
     client.logger.log(`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`, "ready");
 
     // Make the bot "play the game" which is the help command with default prefix.
-    await client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
+    await client.user.setActivity(`${pre.prefix}help`, {type: "PLAYING"});
 };
